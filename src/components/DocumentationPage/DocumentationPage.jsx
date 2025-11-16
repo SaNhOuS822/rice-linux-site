910d5b04792b843b25a814d4d23c0c1b5337da18
SaNhOuS822 <alexander.roshior@mail.ru>
2025-11-17 00:24:41 +0300

    Like a lot of things

--- src/components/DocumentationPage/DocumentationPage.jsx
+++ src/components/DocumentationPage/DocumentationPage.jsx
@@ -3,9 +3,50 @@ import '../../styles/DocumentationPage/DocumentationPage.scss';
 
 
 export default function DocumentationPage() {
+  const check_text = 'Check wiki on GitHub page. ';
+  const read_text = 'Nothing here... ';
+
   return (
-    <div className="documentationPage bg-pink-300 flex items-center justify-center">
-      <h1 className="text-[250px] text-black font-bold text-center">Sory it isn't ready yet :(</h1>
+    <div className="documentationPage bg-pink-300 flex items-center justify-center h-screen w-screen overflow-hidden">
+      <div className="slider relative w-full h-full">
+        {read_text.split('').map((c, i) => {
+          return (
+            <p className="absolute top-1/3 left-1/2 text-white font-bold" key={i} style={{
+              transform: `rotateY(calc(${i}deg * (360 / ${read_text.length}))) translateZ(calc(100vh / 3))`,
+            }}>
+              { c }
+            </p>
+          )
+        })}
+        {read_text.split('').map((c, i) => {
+          return (
+            <p className="absolute top-1/3 left-1/2 text-[#222436] font-bold" key={i} style={{
+              transform: `rotateY(calc(${i}deg * (360 / ${read_text.length}))) translateZ(calc((100vh / 3) - 1px))`,
+            }}>
+              { c }
+            </p>
+          )
+        })}
+
+        {check_text.split('').map((c, i) => {
+          return (
+            <p className="absolute top-1/3 left-1/2 text-white font-bold" key={i} style={{
+              transform: `rotateY(calc(${i}deg * (360 / ${check_text.length}))) translateZ(calc(100vh * 2 / 3))`,
+            }}>
+              { c }
+            </p>
+          )
+        })}
+        {check_text.split('').map((c, i) => {
+          return (
+            <p className="absolute top-1/3 left-1/2 text-[#222436] font-bold" key={i} style={{
+              transform: `rotateY(calc(${i}deg * (360 / ${check_text.length}))) translateZ(calc((100vh * 2 / 3) - 1px))`,
+            }}>
+              { c }
+            </p>
+          )
+        })}
+      </div>
     </div>
   );
 };
--- src/components/DownloadPage/FourthSection.jsx
+++ src/components/DownloadPage/FourthSection.jsx
@@ -1,15 +1,54 @@
 import SectionWrapper from "./SectionWrapper.jsx";
 import RevealingText from './RevealingText';
 
+import { motion } from 'motion/react';
+
 import '../../styles/DownloadPage/FourthSection.scss';
 
 
 export default function FourthSection() {
   return (
     <SectionWrapper className="h-screen w-screen relative">
-      <h2 className="absolute top-[20%] left-0 text-white text-center w-full font-bold text-[100px]">
-        Now Congradulations!
+      <h2 className="absolute top-[20%] left-0 text-center w-full overflow-hidden pb-[10px]">
+        <motion.p className="text-white text-center w-full font-bold text-[150px]"
+          initial={{
+            y: '100%',
+          }}
+          whileInView={{
+            y: 0,
+          }}
+          transition={{
+            duration: 0.7,
+            ease: 'backOut',
+            delay: 0.2,
+          }}
+        >
+          Congradulations!
+        </motion.p>
       </h2>
+      <p className='absolute top-[60%] left-0 w-full overflow-hidden flex justify-center gap-[15px] p-[20px_0px]'>
+        {'Now you can enjoy using Rice Linux!'.split(' ').map((text, i) => {
+          return (
+            <motion.p className='text-[70px] text-white font-bold'
+              initial={{
+                y: (i % 2 == 0) ? '100%' : '-100%',
+                opacity: 0,
+              }}
+              whileInView={{
+                y: 0,
+                opacity: 1,
+              }}
+              transition={{
+                duration: 1,
+                ease: 'backOut',
+                delay: 0.5 + 0.1 * i,
+              }}
+            >
+              { text }
+            </motion.p>
+          )
+        })}
+      </p>
     </SectionWrapper>
   )
 }
