b1b67111d1692020ae993de6e9485abe71dae187
SaNhOuS822 <alexander.roshior@mail.ru>
2025-11-17 18:51:05 +0300

    Added 3D desktop model

--- src/modules/Spline.jsx
+++ src/modules/Spline.jsx
@@ -3,7 +3,9 @@ import Spline from '@splinetool/react-spline';
 
 export default function DesktopModel({ setLoaded }) {
   return (
-    <div className="main__model__wrapper bg-[#222436] h-screen w-screen">
+    <div className="main__model__wrapper bg-[#222436] h-screen w-screen flex items-center justify-center relative">
+      <img src='Untitled.png' className='art absolute w-[60vw] h-[60vh] rounded-[15px] z-20' />
+      <img src='Untitled.png' className='art absolute w-[60vw] h-[60vh] rounded-[15px] z-10 scale-[105%] blur-[40px]' />
       {/* <Spline onLoad={setLoaded(true)} scene="https://draft.spline.design/4Db-kdr8mZorGTFt/scene.splinecode" /> */}
     </div>
   );
